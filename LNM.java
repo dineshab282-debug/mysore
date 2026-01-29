f8bc99d8cd029f99b7dbd580acbfbcfadfb75c3a
dineshab282-debug <dineshab282@gmail.com>
2026-01-29 15:54:52 +0530

    Add files via upload

---
+++ LNM.java
@@ -0,0 +1,8 @@
+public class LNM {
+    public static void main(String[] args) {
+        int a = 10;  
+        int b = 20;  
+        int largest = (a > b) ? a : b;
+        System.out.println("The larger number is: " + largest);
+    }
+}
---
+++ simpleintrest.java
@@ -0,0 +1,9 @@
+public class simpleintrest {
+    public static void main(String[] args) {
+        double principal = 1000.0; 
+        double rate = 5.0;        
+        double time = 2.0;        
+        double simpleInterest = (principal * rate * time) / 100;
+     System.out.println("Simple Interest is: " + simpleInterest);
+    }
+}
